2737f8f950f1c7c64cdcbe615842dce5f3c60755
Rodrigo <rodrigofomo@gmail.com>
2025-08-08 12:03:45 -0300

    Configurando relacionamentos JPA e gerando tabelas de delivery

--- Microservices/delivery-tracking/src/main/java/br/com/rodrigofolha/delivery/delivery_tracking/domain/model/ContactPoint.java
+++ Microservices/delivery-tracking/src/main/java/br/com/rodrigofolha/delivery/delivery_tracking/domain/model/ContactPoint.java
@@ -1,14 +1,19 @@
 package br.com.rodrigofolha.delivery.delivery_tracking.domain.model;
 
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Embeddable
 @EqualsAndHashCode
 @AllArgsConstructor
 @Builder
 @Getter
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
 public class ContactPoint {
     private String zipCode;
     private String street;
--- Microservices/delivery-tracking/src/main/java/br/com/rodrigofolha/delivery/delivery_tracking/domain/model/Delivery.java
+++ Microservices/delivery-tracking/src/main/java/br/com/rodrigofolha/delivery/delivery_tracking/domain/model/Delivery.java
@@ -9,6 +9,14 @@ import java.util.List;
 import java.util.UUID;
 
 import br.com.rodrigofolha.delivery.delivery_tracking.domain.exception.DomainException;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,12 +25,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Setter(AccessLevel.PRIVATE)
 @Getter
 public class Delivery {
 
+    @Id
     @EqualsAndHashCode.Include
     private UUID id;
     private UUID courierId;
@@ -40,9 +50,29 @@ public class Delivery {
 
     private Integer totalItems;
 
+    @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name = "zipCode", column = @Column(name = "sender_zip_code")),
+        @AttributeOverride(name = "street", column = @Column(name = "sender_street")),
+        @AttributeOverride(name = "number", column = @Column(name = "sender_number")),
+        @AttributeOverride(name = "complement", column = @Column(name = "sender_complement")),
+        @AttributeOverride(name = "name", column = @Column(name = "sender_name")),
+        @AttributeOverride(name = "phone", column = @Column(name = "sender_phone"))
+    })
     private ContactPoint sender;
+
+    @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name = "zipCode", column = @Column(name = "recipient_zip_code")),
+        @AttributeOverride(name = "street", column = @Column(name = "recipient_street")),
+        @AttributeOverride(name = "number", column = @Column(name = "recipient_number")),
+        @AttributeOverride(name = "complement", column = @Column(name = "recipient_complement")),
+        @AttributeOverride(name = "name", column = @Column(name = "recipient_name")),
+        @AttributeOverride(name = "phone", column = @Column(name = "recipient_phone"))
+    })
     private ContactPoint recipient;
 
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "delivery")
     private List<Item> items = new ArrayList<>();
 
     public static Delivery draft() {
@@ -57,7 +87,7 @@ public class Delivery {
     }
 
     public UUID addItem(String name, int quantity) {
-        Item item = Item.brandNew(name, quantity);
+        Item item = Item.brandNew(name, quantity, this);
         items.add(item);
         calculateTotalItems();
         return item.getId();
@@ -104,7 +134,7 @@ public class Delivery {
     }
 
     public void markAsDelivered() {
-        this.changeStatusTo(DeliveryStatus.DELIVERY);
+        this.changeStatusTo(DeliveryStatus.DELIVERED);
         this.setFulfilledAt(OffsetDateTime.now());
     }
 
