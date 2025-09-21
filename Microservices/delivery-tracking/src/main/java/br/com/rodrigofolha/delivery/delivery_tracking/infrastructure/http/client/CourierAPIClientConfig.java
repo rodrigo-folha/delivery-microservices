0fb85b05dcdcb0710ed88c44348821996c0ff5ff
Rodrigo <rodrigofomo@gmail.com>
2025-09-21 10:49:19 -0300

    Configurando Eureka

--- Microservices/delivery-tracking/src/main/java/br/com/rodrigofolha/delivery/delivery_tracking/infrastructure/http/client/CourierAPIClientConfig.java
+++ Microservices/delivery-tracking/src/main/java/br/com/rodrigofolha/delivery/delivery_tracking/infrastructure/http/client/CourierAPIClientConfig.java
@@ -1,5 +1,6 @@
 package br.com.rodrigofolha.delivery.delivery_tracking.infrastructure.http.client;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -8,9 +9,16 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
 public class CourierAPIClientConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestClient.Builder loadBalancedRestClienteBuilder() {
+        return RestClient.builder();
+    }
+
     @Bean
     public CourierAPIClient courierAPIClient(RestClient.Builder builder) {
-        RestClient restClient = builder.baseUrl("http://localhost:8081").build();
+        RestClient restClient = builder.baseUrl("http://courier-management").build();
         RestClientAdapter adapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory proxyFactory = HttpServiceProxyFactory.builderFor(adapter).build();
         return proxyFactory.createClient(CourierAPIClient.class);
